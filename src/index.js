8ca5cc5c740a44083148694d52971a5c9980c60c
Austin  Marrazza <austinmarrazza@Austins-MacBook-Pro.local>
2021-11-30 15:16:44 -0800

    completed 4 eventlistners

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import './less/index.less'
 
 // Your code goes here!
+const logoHead = document.querySelector('.logo-heading');
+
+const mousePink = () => {
+    logoHead.style.backgroundColor = 'pink';
+}
+
+logoHead.addEventListener('mouseover', mousePink)
+
+const mouseOut = () => {
+    logoHead.style.backgroundColor = '';
+}
+
+logoHead.addEventListener('mouseout', mouseOut);
+
+const images = document.querySelectorAll('.img-content img')
+
+const bigger = (event,img) => {
+    img.style.width = '60%';
+}
+
+images.forEach(img => {
+    img.addEventListener('dblclick', (event) => bigger(event,img))
+});
